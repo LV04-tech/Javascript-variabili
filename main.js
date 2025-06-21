80dfa1ea632bb711997cbad29c373d1b84f25c11
LV04-tech <millavoinalovych@yahoo.it>
2025-06-21 17:04:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+// 1.dichiarare una variabile num1 e assegnare un numero,visualizzare a schermo
+let num1 = 8;
+console.log(num1);
+
+// 2.dichiarare una variabile num2 e assegnare un numero,visualizzare a schermo
+let num2 = 3;
+console.log(num2);
+
+// 3.dichiarare una variabile stringa e assegnare un valore stringa,visualizzare a schermo
+let stringa = 'ciao a tutti';
+console.log(stringa);
+
+//  4.sostituisci il valore della variabile stringa con un qualsiasi altro valore
+stringa = 10;
+console.log( stringa);
+
+// 5.dichiarare un acostante PIGRECO, e assegnare il valore 3.14159265359
+const PIGRECO = 3.14159265359;
+console.log(PIGRECO);
+
+// 6.prova a cambiare il valore della costante e vedi che succede
+
+PIGRECO = 3.12
+console.log (PIGRECO)
