e8013407d9e798916a5edf5d18cafe9f15e5a04b
LV04-tech <millavoinalovych@yahoo.it>
2025-06-21 19:33:49 +0200

    esercizio operatori1

---
+++ esercizio1.js
@@ -0,0 +1,18 @@
+// Crea 5 variabili contenenti un numero e scrivi un programma in modo da ottenere la somma tra questi numeri e la media.
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let f = 5;
+
+
+let somma = a + b + c + d + f;
+console.log( somma);
+
+let media = somma / 5;
+console.log(media);
+
+// In console poi mostra la seguente frase: ‘La somma tra i numeri equivale a … e la media equivale a…
+
+console.log(`la somma tra i numeri equivale a ${somma}, e la media equivale a ${media}`);
